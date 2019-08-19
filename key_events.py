4822db9aed77f6684985488cbfb0b7e98965dec2
andrwjrdn <andrewjordanreyes@gmail.com>
2019-08-19 14:56:23 -0700

    key events added

---
+++ key_events.py
@@ -0,0 +1,116 @@
+import pygame
+import sys
+
+# IMAGES FOR PIKACHU MOVEMENTS
+
+img_path = './sprites/pikachu/pikachu_'
+f_path = img_path + 'f'
+b_path = img_path + 'b'
+r_path = img_path + 'r'
+l_path = img_path + 'l'
+
+f_images = [f_path+str(f)+'.png' for f in range(7)]
+b_images = [b_path+str(b)+'.png' for b in range(7)]
+r_images = [r_path+str(r)+'.png' for r in range(7)] 
+l_images = [l_path+str(l)+'.png' for l in range(7)]
+
+# IMAGES FOR RAICHU ANIMATED WALKING
+
+img_path = './sprites/raichu/raichu_'
+raichu_f_path = img_path + 'f'
+raichu_b_path = img_path + 'b'
+raichu_r_path = img_path + 'r'
+raichu_l_path = img_path + 'l'
+
+raichu_f_images = [raichu_f_path + str(f) + '.png' for f in range(7)]
+raichu_b_images = [raichu_b_path + str(b) + '.png' for b in range(7)]
+raichu_r_images = [raichu_r_path + str(r) + '.png' for r in range(4)]
+raichu_l_images = [raichu_l_path + str(l) + '.png' for l in range(4)]
+
+class KeyEvents:
+    def __init__(self, PLAYER):
+        self.PLAYER = PLAYER
+        self.counter = 0
+        self.raichu_counter = 0
+        self.raichu_counter_lr = 0
+        self.movement = .25
+        self.orbs = []
+
+    def global_events(self):
+        if self.PLAYER.TRANSFORM:
+            self.movement =  .5
+        else:
+            self.movement = .25
+
+    def quit(self):
+        pygame.quit()
+        sys.exit()
+
+    def key_down(self):
+        self.PLAYER.PLAYER_POS[1] += self.movement
+        self.PLAYER.DIRECTION = 'd'
+
+        self.PLAYER.SPRITE_POS = pygame.image.load(f_images[self.counter])
+        self.counter = (self.counter + 1) % len(f_images)
+
+        if self.PLAYER.TRANSFORM:
+            self.PLAYER.RAICHU  = pygame.image.load(raichu_f_images[self.raichu_counter])
+            self.raichu_counter = (self.raichu_counter + 1) % len(raichu_f_images)
+
+    def key_up(self):
+        self.PLAYER.PLAYER_POS[1] -= self.movement 
+        self.PLAYER.DIRECTION = 'u'
+
+        self.PLAYER.SPRITE_POS = pygame.image.load(b_images[self.counter])
+        self.counter = (self.counter + 1) % len(b_images)
+
+        if self.PLAYER.TRANSFORM:
+            self.PLAYER.RAICHU  = pygame.image.load(raichu_b_images[self.raichu_counter])
+            self.raichu_counter = (self.raichu_counter + 1) % len(raichu_b_images)
+
+    def key_left(self):
+        self.PLAYER.PLAYER_POS[0] -= self.movement 
+        self.PLAYER.DIRECTION = 'l'
+
+        self.PLAYER.SPRITE_POS = pygame.image.load(l_images[self.counter])
+        self.counter = (self.counter + 1) % len(l_images)
+
+        if self.PLAYER.TRANSFORM:
+            self.PLAYER.RAICHU  = pygame.image.load(raichu_l_images[self.raichu_counter_lr])
+            self.raichu_counter_lr = (self.raichu_counter_lr + 1) % len(raichu_l_images)
+
+    def key_right(self):
+        self.PLAYER.PLAYER_POS[0] += self.movement
+        self.PLAYER.DIRECTION = 'r'
+
+        self.PLAYER.SPRITE_POS = pygame.image.load(r_images[self.counter])
+        self.counter = (self.counter + 1) % len(r_images)
+
+        if self.PLAYER.TRANSFORM:
+            self.PLAYER.RAICHU  = pygame.image.load(raichu_r_images[self.raichu_counter_lr])
+            self.raichu_counter_lr = (self.raichu_counter_lr + 1) % len(raichu_r_images)
+
+    def key_space(self):
+        if self.PLAYER.WEAPON:
+            self.PLAYER.PLAYER_INV.remove(self.PLAYER.WEAPON)
+            self.PLAYER.WEAPON.PLACED = True
+
+            # DROP WEAPON LOCATION
+            if self.PLAYER.DIRECTION == 'd':
+                    self.PLAYER.WEAPON.POS[0] = self.PLAYER.PLAYER_POS[0]
+                    self.PLAYER.WEAPON.POS[1] = self.PLAYER.PLAYER_POS[1] - 1
+            elif self.PLAYER.DIRECTION == 'u':
+                    self.PLAYER.WEAPON.POS[0] = self.PLAYER.PLAYER_POS[0]
+                    self.PLAYER.WEAPON.POS[1] = self.PLAYER.PLAYER_POS[1] + 1
+            elif self.PLAYER.DIRECTION == 'r':
+                    self.PLAYER.WEAPON.POS[0] = self.PLAYER.PLAYER_POS[0] - 1
+                    self.PLAYER.WEAPON.POS[1] = self.PLAYER.PLAYER_POS[1]
+            elif self.PLAYER.DIRECTION == 'l':
+                    self.PLAYER.WEAPON.POS[0] = self.PLAYER.PLAYER_POS[0] + 1
+                    self.PLAYER.WEAPON.POS[1] = self.PLAYER.PLAYER_POS[1]
+
+        self.PLAYER.WEAPON = False
+    
+    def key_w(self):
+        self.PLAYER.TRANSFORMING()
+
--- main.py
+++ main.py
@@ -1,172 +1,180 @@
-import pygame
-import sys
+import pygame, sys
 from pygame.locals import *
 from lib import enemies, heroes, items
 from grid import *
 import random
-from key_events import key_events
+from key_events import KeyEvents
 import math
 
-# OBJECTS FOR GAME
-
-PLAYER = heroes.PIKACHU()
-key_events = KeyEvents(Player)
+# INSTANCES OF GAME OBJECTS
+PLAYER = heroes.LINK()
+key_events = KeyEvents(PLAYER)
 WAND = items.WAND()
 GOLD = items.GOLD()
 SWORD = items.SWORD()
+SHIELD = items.SHIELD()
 BOW = items.BOW()
-MEWTWO = enemies.MEWTWO()
+PIKACHU = enemies.PIKACHU()
 PORTAL = enemies.PORTAL()
 TEMPLE = TEMPLE()
 MIDNA = heroes.MIDNA()
 
-# WEAPONS/IN-GAME ITEMS
-
+# GROUPINGS OF RELATED GAME OBJECTS
 GAME_ITEMS = [WAND, SWORD, SHIELD]
 GAME_WEAPONS = [WAND, BOW]
-koffing_LIST = []
+KOFFING_LIST = []
 orbs_list = []
 
-# TIMER FOR SPECIFIC IN-GAME EVENTS
+# OTHER CONFIG
+INVFONT = pygame.font.SysFont('FreeSansBold.ttf', 20)
+HEALTHFONT = pygame.font.SysFont('FreeSansBold.ttf', 40)
+portal_path = './textures/portal/portal_'
+portal_images = [portal_path + str(p) + '.png' for p in range(1, 7)]
 
-# MEWTWO MECHANICS/MOVEMENT
+"""
+TIMED EVENTS
+"""
+# PIKACHU MOVEMENT
 pygame.time.set_timer(USEREVENT, 400)
 # SPAWN KOFFING
 pygame.time.set_timer(USEREVENT + 1, 7500)
 # INCREMENT KOFFING PORTAL FRAMES
 pygame.time.set_timer(USEREVENT + 2, 400)
-# KOFFING MECHANICS/MOVEMENTS
+# MOVE KOFFING
 pygame.time.set_timer(USEREVENT + 3, 1000)
 # ORB TRAVEL ON PATH
 pygame.time.set_timer(USEREVENT + 4, 100)
 
 GAME_OVER = False
-
-# OVERALL GAME MECHANICS/FLOW
-
+# GAME LOOP
 while not GAME_OVER:
 
-  MEWTWO_VUNERABLE_IF = [
-      koffing for koffing in KOFFING_LIST if koffing.APPEAR == True]
+    PIKACHU_VULNERABLE_IF = [koffing for koffing in KOFFING_LIST if koffing.APPEAR == True]
 
-  if len(MEWTWO_VULNERABLE_IF) < 1:
-    MEWTWO.VULNERABLE = True
-  else:
-    MEWTWO.VULNERABLE = False
+    if len(PIKACHU_VULNERABLE_IF) < 1:
+        PIKACHU.VULNERABLE = True
+    else:
+        PIKACHU.VULNERABLE = False
 
-  for event in pygame.event.get():
+    for event in pygame.event.get():
 
-    keys = pygame.key.get_pressed()
-    key_events.global_events()
+        keys = pygame.key.get_pressed()
+        key_events.global_events()
+    
+        if event.type == QUIT:
+            key_events.quit()
+    
+        if keys[K_w] and keys[K_t]:
+            key_events.key_w()
 
-    if event.type == QUIT:
-      key_events.quit()
-
-# MOVE RIGHT
-  if (keys[K_RIGHT]) and PLAYER.PLAYER_POS[0] < MAPWIDTH - 1:
-           key_events.key_right()
-
-# MOVE LEFT
-  if (keys[K_LEFT]) and PLAYER.PLAYER_POS[0] > 0:
-           key_events.key_left()
-
-# MOVE UP
-  if (keys[K_UP]) and PLAYER.PLAYER_POS[1] > 0:
+        # MOVE RIGHT
+        if (keys[K_RIGHT]) and PLAYER.PLAYER_POS[0] < MAPWIDTH - 1:
+           key_events.key_right() 
+    
+        # MOVE LEFT
+        if (keys[K_LEFT]) and PLAYER.PLAYER_POS[0] > 0:
+           key_events.key_left() 
+    
+        # MOVE UP
+        if (keys[K_UP]) and PLAYER.PLAYER_POS[1] > 0:
             key_events.key_up()
-
-# MOVE DOWN
-  if (keys[K_DOWN]) and PLAYER.PLAYER_POS[1] < MAPHEIGHT - 1:
+    
+        # MOVE DOWN
+        if (keys[K_DOWN]) and PLAYER.PLAYER_POS[1] < MAPHEIGHT - 1:
             key_events.key_down()
+    
+        # PLACING DOWN ITEMS
+        if (keys[K_SPACE]):
+            key_events.key_space()
+    
+        # FIRE ORB FROM WAND
+        if (keys[K_f]):
+            if PLAYER.WEAPON == WAND:
+                orbs_list.append(heroes.ORB(math.ceil(PLAYER.PLAYER_POS[0]), math.ceil(PLAYER.PLAYER_POS[1]), PLAYER.DIRECTION))
 
-# PICK UP ITEMS WITH SPACEBAR
-if (keys[K_SPACE]):
-  key_events.key_space()
+        """
+        TIMED EVENTS
+        """
 
-# FIRE WEAPON FROM ITEM
+        # PIKACHU W/PORTAL MOVEMENT
+        if (event.type == USEREVENT):
+            if PORTAL.FRAME < 5:
+                PORTAL.FRAME += 1
+            else:
+                x = random.randint(1, 9)
+                y = random.randint(1, 9)
+                PORTAL.POS = [x, y]
+                PIKACHU.PIKACHU_POS = [x, y]
+                PORTAL.FRAME = 1
+        
+        # KOFFING OBJECT GENERATOR 
+        elif (event.type == USEREVENT + 1):
+            NEW_KOFFING = enemies.KOFFING()
+            NEW_KOFFING.PORTAL = enemies.PORTAL()
+            KOFFING_LIST.append(NEW_KOFFING)
 
-if (keys[K_f]):
-  if PLAYER.WEAPON == WAND:
-      orbs_list.append(heroes.ORB(math.ceil(PLAYER.PLAYER_POS[0]), math.ceil(
-          PLAYER.PLAYER_POS[1]), PLAYER.DIRECTION))
-
-
-# MEWTWO W/ PORTAL MOVEMENT
-if (event.type == USEREVENT):
-  if PORTAL.FRAME < 5;
-      PORTAL.FRAME += 1
-  else:
-    x = random.randint(1, 9)
-    y = random.randint(1, 9)
-    PORTAL.POS = [x, y]
-    MEWTWO.MEWTWO_POS = [x, y]
-    PORTAL.FRAME = 1
-
-# KOFFING OBJECT GENERATOR
-elif (event.type == USEREVENT + 1):
-  NEW_KOFFING = enemies.KOFFING()
-  NEW_KOFFING.PORTAL = enemies.PORTAL()
-  KOFFING_LIST.append(NEW_KOFFING)
-
-# KOFFING W/ PORTAL GENERATOR
-elif (event.type == USEREVENT + 2):
-  for koffing in KOFFING_LIST:
-    if koffing.PORTAL_APPEAR and koffing.PORTAL.FRAME < 5:
+       # KOFFING W/PORTAL GENERATOR 
+        elif (event.type == USEREVENT + 2):
+            for koffing in KOFFING_LIST:
+                if koffing.PORTAL_APPEAR and koffing.PORTAL.FRAME < 5:
                     koffing.PORTAL.FRAME += 1
-    elif not koffing.SUMMONED:
-          koffing.PORTAL_APPEAR = False
-          koffing.APPEAR = True
-          koffing.SUMMONED = True
-          koffing.POS = [koffing.PORTAL.POS[0], koffing.PORTAL.POS[1]]
-
-# KOFFING MOVEMENTS TO DAMAGE PLAYER
-elif (event.type == USEREVENT + 3):
+                elif not koffing.SUMMONED:
+                    koffing.PORTAL_APPEAR = False
+                    koffing.APPEAR = True
+                    koffing.SUMMONED = True
+                    koffing.POS = [koffing.PORTAL.POS[0], koffing.PORTAL.POS[1]]
+        
+        # KOFFINGS MOVEMENTS HUNT PLAYER
+        elif (event.type == USEREVENT + 3):
             for koffing in KOFFING_LIST:
                 if koffing.APPEAR:
                     if PLAYER.PLAYER_POS == koffing.POS:
                         PLAYER.HEALTH -= 10
                     for coordinate in range(len(koffing.POS)):
                         if PLAYER.PLAYER_POS[coordinate] > koffing.POS[coordinate]:
-                            koffing.POS[coordinate] += 1
+                            koffing.POS[coordinate] += 1 
                         else:
                             koffing.POS[coordinate] -= 1
-
-# MOVEMENT OF ORB MECHANICS
-elif (event.type == USEREVENT + 4):
+        
+        # ORB PATH MOVEMENT ANIMATION
+        elif (event.type == USEREVENT + 4):
             for orb in orbs_list:
                 if orb.DIRECTION == 'd':
                     orb.POS[1] += 1
                 elif orb.DIRECTION == 'u':
                     orb.POS[1] -= 1
                 elif orb.DIRECTION == 'l':
-                    orb.POS[0] -= 1
+                    orb.POS[0] -= 1 
                 elif orb.DIRECTION == 'r':
                     orb.POS[0] += 1
 
-# ITEM CONDITIONS
-  for item in GAME_ITEMS:
+        # PICKUP ITEM CONDITIONS
+        for item in GAME_ITEMS:
             if PLAYER.PLAYER_POS == item.POS and item.PLACED:
                 PLAYER.PLAYER_INV.append(item)
                 item.PLACED = False
                 if item in GAME_WEAPONS:
                     PLAYER.WEAPON = item
 
-# RENDERING THE GRID FOR GAME
+    """
+    RENDERING GRID, SPRITES, AND VIEWS
+    """
 
-for row in range(MAPHEIGHT):
-  for column in range(MAPWIDTH):
-      DISPLAYSURFACE.blit(TEXTURES[GRID[row] [column]], (column*TILESIZE, row*TILESIZE))
+    # RENDER GAME GRID
+    for row in range(MAPHEIGHT):
+        for column in range(MAPWIDTH):
+            DISPLAYSURFACE.blit(TEXTURES[GRID[row][column]], (column*TILESIZE, row*TILESIZE))
 
-# RENDER PIKACHU
-
-if PLAYER.TRANSFORM:
-    DISPLAYSURFACE.blit(PLAYER.WOLF, (PLAYER.PLAYER_POST[0]*TILESIZE, PLAYER.PLAYER_POS[1]*TILESIZE))
-else:
+    # RENDER LINK
+    if PLAYER.TRANSFORM:
+        DISPLAYSURFACE.blit(PLAYER.WOLF, (PLAYER.PLAYER_POS[0]*TILESIZE, PLAYER.PLAYER_POS[1]*TILESIZE))
+    else:
         DISPLAYSURFACE.blit(PLAYER.SPRITE_POS, (PLAYER.PLAYER_POS[0]*TILESIZE, PLAYER.PLAYER_POS[1]*TILESIZE))
 
-# RENDER POKEGYM
-DISPLAYSURFACE.blit(TEMPLE.SPRITE, (TEMPLE.X_POS*TILESIZE, TEMPLE.Y_POS*TILESIZE))
+    # RENDER TEMPLE
+    DISPLAYSURFACE.blit(TEMPLE.SPRITE, (TEMPLE.X_POS*TILESIZE, TEMPLE.Y_POS*TILESIZE))
 
-# RENDER MIDNA
+    # RENDER MIDNA
     MIDNA.APPEARED = True
     if MIDNA.APPEARED:
         if PLAYER.TRANSFORM:
@@ -192,9 +200,9 @@ DISPLAYSURFACE.blit(TEMPLE.SPRITE, (TEMPLE.X_POS*TILESIZE, TEMPLE.Y_POS*TILESIZE
 
     # RENDER ORBS
     for orb in orbs_list:
-        if orb.POS == MEWTWO.MEWTWO_POS and MEWTWO.VULNERABLE:
-            print('MEWTWO HEALTH', MEWTWO.HEALTH)
-            MEWTWO.HEALTH -= 10
+        if orb.POS == PIKACHU.PIKACHU_POS and PIKACHU.VULNERABLE:
+            print('PIKACHU HEALTH', PIKACHU.HEALTH)
+            PIKACHU.HEALTH -= 10
         for koffing in KOFFING_LIST:
                 if orb.POS == koffing.POS:
                     koffing.APPEAR = False
@@ -219,23 +227,23 @@ DISPLAYSURFACE.blit(TEMPLE.SPRITE, (TEMPLE.X_POS*TILESIZE, TEMPLE.Y_POS*TILESIZE
     DISPLAYSURFACE.blit(PLAYER_HEALTH_BAR_TEXT, (15, MAPHEIGHT*TILESIZE-500))
     DISPLAYSURFACE.blit(HEALTHFONT.render(str(PLAYER.HEALTH), True, GREEN, BLACK), (225, MAPHEIGHT*TILESIZE - 500))
 
-    # RENDER MEWTWO HEALTH BAR
-    PLAYER_MANA_BAR_TEXT = HEALTHFONT.render('MEWTWO HEALTH:', True, RED, BLACK)
+    # RENDER PIKACHU HEALTH BAR
+    PLAYER_MANA_BAR_TEXT = HEALTHFONT.render('PIKACHU HEALTH:', True, RED, BLACK)
     DISPLAYSURFACE.blit(PLAYER_MANA_BAR_TEXT, (650, MAPHEIGHT*TILESIZE-500))
-    DISPLAYSURFACE.blit(HEALTHFONT.render(str(MEWTWO.HEALTH), True, RED, BLACK), (900, MAPHEIGHT*TILESIZE-500))
+    DISPLAYSURFACE.blit(HEALTHFONT.render(str(PIKACHU.HEALTH), True, RED, BLACK), (900, MAPHEIGHT*TILESIZE-500))
 
     # RENDER TREES
     for tree in sorted(trees, key=lambda t: t.Y_POS):
         DISPLAYSURFACE.blit(tree.SPRITE, (tree.X_POS, tree.Y_POS))
 
-    # RENDER MEWTWO AND PORTAL
-    DISPLAYSURFACE.blit(pygame.image.load(portal_images[PORTAL.FRAME]), (MEWTWO.MEWTWO_POS[0]*TILESIZE, MEWTWO.MEWTWO_POS[1]*TILESIZE))
-    DISPLAYSURFACE.blit(MEWTWO.MEWTWO, (MEWTWO.MEWTWO_POS[0]*TILESIZE, MEWTWO.MEWTWO_POS[1]*TILESIZE))
+    # RENDER PIKACHU AND PORTAL
+    DISPLAYSURFACE.blit(pygame.image.load(portal_images[PORTAL.FRAME]), (PIKACHU.PIKACHU_POS[0]*TILESIZE, PIKACHU.PIKACHU_POS[1]*TILESIZE))
+    DISPLAYSURFACE.blit(PIKACHU.PIKACHU, (PIKACHU.PIKACHU_POS[0]*TILESIZE, PIKACHU.PIKACHU_POS[1]*TILESIZE))
 
     
     pygame.display.update()
 
-    if MEWTWO.HEALTH <= 0:
+    if PIKACHU.HEALTH <= 0:
         GAME_OVER = True
         print('GAME OVER, YOU WIN!')
     
@@ -244,7 +252,5 @@ DISPLAYSURFACE.blit(TEMPLE.SPRITE, (TEMPLE.X_POS*TILESIZE, TEMPLE.Y_POS*TILESIZE
         print('GAME OVER, YOU LOSE')
 
 # END OF GAME LOOP
-
-
 
 
